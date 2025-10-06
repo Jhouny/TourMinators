7bb32ab0e4d42f2552fe27ac51e42e29d03cede9
Imane Kenza <imane.taarabit@insa-lyon.fr>
2025-10-06 17:43:24 +0200

    changes in main class

--- src/main/backend/Main.java
+++ src/main/backend/Main.java
@@ -7,8 +7,9 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Map<Long, Node> nodes = XMLParser.parseNodes("nodes.xml");
-        List<Edge> edges = XMLParser.parseEdges("edges.xml");
+        String xmlPath = "src/ressources/petitPlan.xml";
+        Map<Long, Node> nodes = XMLParser.parseNodes(xmlPath);
+        List<Edge> edges = XMLParser.parseEdges(xmlPath);
         System.out.println("Nodes: " + nodes.size());
         System.out.println("Edges: " + edges.size());
     }
