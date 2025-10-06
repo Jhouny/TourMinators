30ef4e811fc9c0484ef68524fc44e3c301e22647
aguyonnaud <agnes.guyonnaud@insa-lyon.fr>
2025-10-06 17:48:05 +0200

    differents cas RunTSP

--- TSP_Basic_Implementation/tsp/RunTSP.java
+++ TSP_Basic_Implementation/tsp/RunTSP.java
@@ -3,8 +3,20 @@ package tsp;
 public class RunTSP {
 	
 	public static void main(String[] args) {
-		TSP tsp = new TSP3();
-		for (int nbSommets = 8; nbSommets <= 16; nbSommets += 2){
+		TSP tsp;
+		switch args[0] {
+			case "1":
+				tsp = new TSP1();
+				break;
+			case "2":
+				tsp = new TSP2();
+				break;
+			case "3":
+				tsp = new TSP3();
+				break;
+		}
+
+		for (int nbSommets = 16; nbSommets <= 32; nbSommets += 2){
 			System.out.println("Graphes de "+nbSommets+" sommets :");
 			Graphe g = new GrapheComplet(nbSommets);
 			long tempsDebut = System.currentTimeMillis();
