e0f1060cecbc0dd39348fddaec848c149f258549
Imane Kenza <imane.taarabit@insa-lyon.fr>
2025-10-06 11:17:22 +0200

    added lengh in edge

--- src/main/backend/models/Edge.java
+++ src/main/backend/models/Edge.java
@@ -4,11 +4,14 @@ class Edge {
 
     long originId;
     long destinationId;
+    float length;
     String streetName;
 
-    public Edge(long originId, long destinationId, String streetName) {
+    public Edge(long originId, long destinationId, float length, String streetName) {
         this.originId = originId;
         this.destinationId = destinationId;
+        this.length = length;
         this.streetName = streetName;
+        
     }
 }
