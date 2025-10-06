c510c62849e0b4e942e8ebf1aa72774d6aba523d
aguyonnaud <agnes.guyonnaud@insa-lyon.fr>
2025-10-06 11:36:35 +0200

    tdd and features

---
+++ src/main/backend/PathCalculator.java
@@ -0,0 +1,10 @@
+public class PathCalculator {
+    public int addition(int a, int b) {
+        return a + b;
+    }
+
+    public int division(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Division par zéro interdite");
+        return a / b;
+    }
+}
