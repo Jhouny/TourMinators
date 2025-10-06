16c866fbc69106ab124b05ff565e30775b4967a9
aguyonnaud <agnes.guyonnaud@insa-lyon.fr>
2025-10-06 17:06:08 +0200

    amelioration bound du tsp

--- TSP_Basic_Implementation/tsp/TSP2.java
+++ TSP_Basic_Implementation/tsp/TSP2.java
@@ -4,12 +4,14 @@ import java.util.Collection;
 import java.util.Iterator;
 
 public class TSP2 extends TemplateTSP {
-	
-	protected Graphe g;
 
 	@Override
 	protected int bound(Integer sommetCourant, Collection<Integer> nonVus) {
 		// on veut une borne inferieure la plus haute possible du cout des chemins passant par tous les sommets de nonVus
+		//todo : récupérer g
+		//Graphe g = this.g;
+
+		 // on prend la longeur pour arriver au sommet 0 en passant par le sommet le plus loin des sommets restants
 		if (nonVus.size() == 0) return g.getCout(sommetCourant,0);
 		else {
 			// on prend la longeur pour arriver au sommet 0 en passant par le sommet le plus loin des sommets restants
