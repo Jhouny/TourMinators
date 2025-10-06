b7ea110e85f63d02b3080187c61011bf5c9ce3f6
Imane Kenza <imane.taarabit@insa-lyon.fr>
2025-10-06 10:44:59 +0200

    parsers

---
+++ src/main/backend/class/noeud.java
@@ -0,0 +1,11 @@
+class Noeud {
+    long id;
+    double latitude;
+    double longitude;
+
+    public Noeud(long id, double latitude, double longitude) {
+        this.id = id;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
---
+++ src/main/backend/class/troncon.java
@@ -0,0 +1,11 @@
+class Troncon {
+    long origineId;
+    long destinationId;
+    String nomRue;
+
+    public Troncon(long origineId, long destinationId, String nomRue) {
+        this.origineId = origineId;
+        this.destinationId = destinationId;
+        this.nomRue = nomRue;
+    }
+}
