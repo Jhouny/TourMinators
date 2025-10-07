fe98d86d364b1ed8df9633690b8c10c71abd4f00
aguyonnaud <agnes.guyonnaud@insa-lyon.fr>
2025-10-07 11:47:50 +0200

    corrictions switch

--- TSP_Basic_Implementation/tsp/RunTSP.java
+++ TSP_Basic_Implementation/tsp/RunTSP.java
@@ -4,19 +4,19 @@ public class RunTSP {
 	
 	public static void main(String[] args) {
 		TSP tsp;
-		switch args[0] {
+		switch (args[0]) {
 			case "1":
 				tsp = new TSP1();
 				break;
 			case "2":
 				tsp = new TSP2();
 				break;
-			case "3":
+			default :
 				tsp = new TSP3();
 				break;
 		}
 
-		for (int nbSommets = 16; nbSommets <= 32; nbSommets += 2){
+		for (int nbSommets = 8; nbSommets <= 22; nbSommets += 2){
 			System.out.println("Graphes de "+nbSommets+" sommets :");
 			Graphe g = new GrapheComplet(nbSommets);
 			long tempsDebut = System.currentTimeMillis();
