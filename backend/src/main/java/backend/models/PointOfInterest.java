92596c3f8363306a800870b89d85a80e21ce3f01
aguyonnaud <agnes.guyonnaud@insa-lyon.fr>
2025-10-13 17:18:51 +0200

    enum type poi

--- backend/src/main/java/backend/models/PointOfInterest.java
+++ backend/src/main/java/backend/models/PointOfInterest.java
@@ -3,18 +3,24 @@ package backend.models;
 public class PointOfInterest {
 
     private Node node;
-    private String type; // evolution : enum
+    private PoIEnum type; // evolution : enum
     private Long associatedPoI; // id of associated point of interest (null if type = warehouse)
     private int duration; // in seconds, time needed to pickup/deliver, 0 if warehouse
 
-    public PointOfInterest(Node node, String type, Long associatedPoI, int duration) {
+    public enum PoIEnum {
+        WAREHOUSE,
+        PICKUP,
+        DELIVERY;
+    }
+
+    public PointOfInterest(Node node, PoIEnum type, Long associatedPoI, int duration) {
         this.node = node;
         this.type = type;
         this.associatedPoI = associatedPoI;
         this.duration = duration;
     }
 
-    public String getType() {
+    public PoIEnum getType() {
         return type;
     }
 
