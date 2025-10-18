897c6698570fa5de9d990a55fbf59aad807ad368
aguyonnaud <agnes.guyonnaud@insa-lyon.fr>
2025-10-18 12:10:17 +0200

    begin testServer

---
+++ backend/src/test/java/com/tourminators/backend/ServerTest.java
@@ -0,0 +1,67 @@
+package com.tourminators.backend;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import backend.TSP.TSP;
+import backend.TSP.TSP2;
+import backend.models.Edge;
+import backend.models.Node;
+import backend.models.Pair;
+import backend.models.PointOfInterest;
+import backend.models.TSPRequest;
+import backend.Server;
+import backend.TSP.Graph;
+
+
+public class ServerTest {
+
+   @Test
+   public void serverShouldReturnBestSolutionsAndPredecessors() {
+
+        Server server = new Server();
+        TSPRequest request =  new TSPRequest();
+
+        // Map<Long, Node> nodes = new HashMap<>();
+        // nodes.put(Long.valueOf(0), new Node(0, 45.751904, 4.857877));
+        // nodes.put(Long.valueOf(1), new Node(1, 45.752000, 4.860000));
+        // nodes.put(Long.valueOf(2), new Node(2, 45.753000, 4.861000));
+
+        // List<Edge> edges = new ArrayList<>();
+        // edges.add(new Edge(Long.valueOf(0), Long.valueOf(1), 250.0f, "edge_0_1"));
+        // edges.add(new Edge(Long.valueOf(1), Long.valueOf(2), 300.0f, "edge_1_2"));
+        // edges.add(new Edge(Long.valueOf(2), Long.valueOf(0), 350.0f, "edge_2_0"));
+
+
+        // Map<Long, PointOfInterest> tour = new HashMap<>();
+        // tour.put(Long.valueOf(0), new PointOfInterest(nodes.get(Long.valueOf(0)), PointOfInterest.PoIEnum.WAREHOUSE, null, 0));
+        // tour.put(Long.valueOf(1), new PointOfInterest(nodes.get(Long.valueOf(1)), PointOfInterest.PoIEnum.DELIVERY, Long.valueOf(2), 200));
+        // tour.put(Long.valueOf(2), new PointOfInterest(nodes.get(Long.valueOf(2)), PointOfInterest.PoIEnum.PICKUP, Long.valueOf(1), 300));
+
+
+        server.runTSP(request);
+        Pair<Long, LocalTime>[] bestSolution = (Pair<Long, LocalTime>[]) new Pair[4];
+
+        bestSolution[0] = new Pair(Long.valueOf(0), LocalTime.of(8, 0));
+        bestSolution[1] = new Pair(Long.valueOf(2), LocalTime.of(8, 0).plusSeconds(Long.valueOf(132)));
+        bestSolution[2] = new Pair(Long.valueOf(1), LocalTime.of(8, 0).plusSeconds(Long.valueOf(576)));
+        bestSolution[3] = new Pair(Long.valueOf(0), LocalTime.of(8, 0).plusSeconds(Long.valueOf(932)));
+
+
+        Map<String, Object> expectedResponse = Map.of(
+                "bestSolution", bestSolution,
+                "predecesseurs", g.getPredecessors());
+
+        Assert.assertEquals(expectedResponse, server.runTSP(request).getBody());
+
+		// Graph g = new Graph(nodes, edges, tour);
+		
+
+    }
+}
